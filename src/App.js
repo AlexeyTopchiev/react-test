6cad54540ab01d3e81ad037827802523795c8a6f
AlexeyTopchiev <wearelols001@gmail.com>
2021-11-29 02:22:02 +0300

    Frontend: add basic sort and filter algs in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react"
+import React, { useState, useMemo } from "react"
 import "./styles/App.css"
 import PostList from "./components/PostList"
 import PostForm from "./components/PostForm"
 import MySelect from "./components/UI/select/MySelect"
-// import MyInput from "./components/UI/input/MyInput"
+import MyInput from "./components/UI/input/MyInput"
 
 function App() {
   const [posts, setPosts] = useState([
@@ -11,9 +11,26 @@ function App() {
     { id: 2, title: "aa", body: "ff" },
     { id: 3, title: "cc", body: "dd" }
   ])
-
   const [selectedSort, setSelectedSort] = useState("")
-  // const [searchQuery, setSearchQuery] = useState("")
+  const [searchQuery, setSearchQuery] = useState("")
+
+  const sortedPosts = useMemo(() => {
+    console.log("sortedPosts run")
+    if (selectedSort) {
+      return [...posts].sort((a, b) =>
+        a[selectedSort].localeCompare(b[selectedSort])
+      )
+    } else {
+      return posts
+    }
+  }, [posts, selectedSort])
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    console.log("sortedAndSearchedPosts run")
+    return sortedPosts.filter(post =>
+      post.title.toLowerCase().includes(searchQuery.toLowerCase())
+    )
+  }, [searchQuery, sortedPosts])
 
   const createPost = newPost => {
     setPosts([...posts, newPost])
@@ -25,19 +42,18 @@ function App() {
 
   const sortPosts = sort => {
     setSelectedSort(sort)
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
   }
 
   return (
     <div className="App">
       <PostForm create={createPost} />
       <hr style={{ margin: "15px 0" }} />
-      {/* <MyInput
+      <MyInput
         value={searchQuery}
         onChange={e => setSearchQuery(e.target.value)}
         placeholder="Поиск"
         type="text"
-      /> */}
+      />
       <MySelect
         defaultValue="Сортировка"
         options={[
@@ -47,8 +63,12 @@ function App() {
         onChange={sortPosts}
         value={selectedSort}
       />
-      {posts.length ? (
-        <PostList remove={removePost} posts={posts} title="Посты про JS" />
+      {sortedAndSearchedPosts.length ? (
+        <PostList
+          remove={removePost}
+          posts={sortedAndSearchedPosts}
+          title="Посты про JS"
+        />
       ) : (
         <h1 style={{ textAlign: "center" }}>Посты не найдены</h1>
       )}
