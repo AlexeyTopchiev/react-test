cfa47b10574f7d9c1213238524f9e8b7dbe03838
AlexeyTopchiev <wearelols001@gmail.com>
2021-12-09 02:24:00 +0300

    Frontend: add request for comments by post

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -7,28 +7,46 @@ import MyLoader from "../components/UI/loader/MyLoader"
 const PostIdPage = () => {
   const params = useParams()
   console.log("params", params)
-
   const [post, setPost] = useState({})
+  const [comments, setComments] = useState([])
   const [fetchPostById, isPostLoading, postError] = useFetching(async id => {
     const response = await PostService.getById(id)
     setPost(response.data)
   })
 
-  console.log("post", post)
+  const [fetchComments, isComLoading, comError] = useFetching(async id => {
+    const response = await PostService.getCommentsByPostId(id)
+    setComments(response.data)
+  })
+
   useEffect(() => {
     fetchPostById(params.id)
+    fetchComments(params.id)
   }, [])
 
   return (
     <div>
       <h1>You're open page post id = {params.id} </h1>
       {postError && <h2>Произошла ошибка: {postError}</h2>}
-
       {isPostLoading ? (
         <MyLoader />
       ) : (
         <div>
           {post.id}. {post.title}
+        </div>
+      )}
+      <h2>Комментарии:</h2>
+      {comError && <h2>Произошла ошибка: {comError}</h2>}
+      {isComLoading ? (
+        <MyLoader />
+      ) : (
+        <div>
+          {comments.map(comm => (
+            <div style={{ marginTop: "15px" }} key={comm.id}>
+              <h5>{comm.email}</h5>
+              <div>{comm.body}</div>
+            </div>
+          ))}
         </div>
       )}
     </div>
